caf5083fa955fcb9ffd2f065a5e71907386cd634
Takao_Etogit config --global user.email takaoetoooo.@gmail.comgit config --listgit config --listfpath=(~/.zsh /Users/etotakao/.zsh /Users/etotakao/.zsh /Users/etotakao/.zsh /usr/local/share/zsh/site-functions /usr/share/zsh/site-functions /usr/share/zsh/5.8.1/functions)zstyle ':completion:*:*:git:*' script ~/.zsh/git-completion.bashautoload -Uz compinit && compinitgit config --global user.name Takao_Eto <takaoetoooo@gmail.com>
2025-01-19 16:50:47 +0900

    tamesi

---
+++ src/main/java/com/example/tamesi/Domain/Tamesi.java
@@ -0,0 +1,24 @@
+package com.example.tamesi.Domain;
+
+public class Tamesi {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "TamesiForm [id=" + id + ", name=" + name + "]";
+    }
+}
