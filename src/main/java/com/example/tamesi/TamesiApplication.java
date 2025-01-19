48179ec7fb00e72b3aaff5c5cbe2cec0d01cf2a3
Takao_Etogit config --global user.email takaoetoooo.@gmail.comgit config --listgit config --listfpath=(~/.zsh /Users/etotakao/.zsh /Users/etotakao/.zsh /Users/etotakao/.zsh /usr/local/share/zsh/site-functions /usr/share/zsh/site-functions /usr/share/zsh/5.8.1/functions)zstyle ':completion:*:*:git:*' script ~/.zsh/git-completion.bashautoload -Uz compinit && compinitgit config --global user.name Takao_Eto <takaoetoooo@gmail.com>
2025-01-19 16:15:50 +0900

    first commit

---
+++ src/main/java/com/example/tamesi/TamesiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tamesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamesiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tamesi/TamesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tamesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
